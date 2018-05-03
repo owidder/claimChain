7983fd54698aa4ca15ecda7612c8576a80cf7b09
Oliver Widder <oliver.widder@gmail.com>
2018-05-03 20:01:07 +0200

    sorted events

--- server/eventServer.js
+++ server/eventServer.js
@@ -12,15 +12,44 @@ const newEvent = (event) => {
     webSocketServer.sendObjectToAllSockets(event);
 }
 
-eventsToListenTo.forEach((eventName) => {
-    eventsUtil.pastEvents(contracts.ChainTraze, eventName).on('event', (events) => {
-        events.forEach((event) => {
-            newEvent(event);
-        })
+const eventComparator = (e1, e2) => {
+    if(e1.blockNumber !== e2.blockNumber) {
+        return e1.blockNumber > e2.blockNumber ? 1 : -1;
+    }
+    else if(e1.transactionIndex !== e2.transactionIndex) {
+        return  e1.transactionIndex > e2.transactionIndex ? 1 : -1;
+    }
+    else if(e1.logIndex !== e2.logIndex) {
+        return e1.logIndex > e2.logIndex ? 1 : -1;
+    }
+
+    return 0;
+}
+
+const getPastEventsRecursive = (eventNames, index, resolve, allEvents) => {
+    if(index < eventNames.length) {
+        const eventName = eventNames[index];
+        eventsUtil.pastEvents(contracts.ChainTraze, eventName).on('event', (events) => {
+            Array.prototype.push.apply(allEvents, events);
+            getPastEventsRecursive(eventNames, index+1, resolve, allEvents);
+        });
+    }
+    else {
+        resolve(allEvents);
+    }
+}
+
+new Promise((resolve) => {
+    getPastEventsRecursive(eventsToListenTo, 0, resolve, []);
+}).then((allEvents) => {
+    allEvents.sort(eventComparator).forEach((event) => {
+        newEvent(event);
     });
 
-    eventsUtil.subscribe(contracts.ChainTraze, eventName).on('event', (event) => {
-        newEvent(event);
+    eventsToListenTo.forEach((eventName) => {
+        eventsUtil.subscribe(contracts.ChainTraze, eventName).on('event', (event) => {
+            newEvent(event);
+        });
     });
 })
 
